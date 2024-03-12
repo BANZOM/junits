a2a52e01178042e3cda8c1945de59370eca3dc39
Aditya Panwar <adityap@cabin4j.com>
2024-03-12 18:24:57 +0530

    Update JUnit imports

--- src/test/java/com/banzo/junitexamples/CalcTest.java
+++ src/test/java/com/banzo/junitexamples/CalcTest.java
@@ -1,7 +1,8 @@
 package com.banzo.junitexamples;
-import static org.junit.Assert.*;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class CalcTest {
     @Test
