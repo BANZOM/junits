2d5009d8be5e775813cfbdec890134348f03987c
Aditya Panwar <adityap@cabin4j.com>
2024-03-12 23:48:09 +0530

    Add test for array copy performance

--- src/test/java/com/banzo/junitexamples/MiscellaneousTest.java
+++ src/test/java/com/banzo/junitexamples/MiscellaneousTest.java
@@ -2,6 +2,8 @@ package com.banzo.junitexamples;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.Test;
 
 public class MiscellaneousTest {
@@ -11,5 +13,17 @@ public class MiscellaneousTest {
         int[] actual = {1, 2, 3};
 
         assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test2() {
+        int[] actual = {1, 2, 3};
+
+        assertTimeout(Duration.ofMillis(100), () -> {
+            for (int i = 0; i < 1000000; i++) {
+                int[] copy = new int[actual.length];
+                System.arraycopy(actual, 0, copy, 0, actual.length);
+            }
+        });
     }
 }
